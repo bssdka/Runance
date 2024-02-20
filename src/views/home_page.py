30d211e9f1cc381024230dcf67bb0c32960449f1
SenyDev <160341122+bssdka@users.noreply.github.com>
2024-02-21 01:02:54 +0300

    Add files via upload

---
+++ src/views/home_page.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import customtkinter as tk
+# bg-color #3F3A46
+
+class Content(tk.CTkFrame):
+    def __init__(self, master, width, height):
+        super().__init__(master, width, height)
+
+        main_frame = tk.CTkFrame(self, fg_color="#3F3A46", width=330, height=470).place(x=0, y=0)
+        
+        lbl = tk.CTkLabel(self, text="HOME PAGE")
+        lbl.place(x=50, y=50)
+
+        
