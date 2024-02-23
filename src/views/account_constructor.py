c0462c0aa54cb5d1de23c2afa9602095fc4dfa38
bssdka <ay.zhilyaev@outlook.com>
2024-02-23 15:14:54 +0300

    update

--- src/views/account_constructor.py
+++ src/views/account_constructor.py
@@ -4,8 +4,6 @@ import customtkinter as tk
 # bg-color #3F3A46
 
 import views.settings.config as config
-import views.account_functions.sign_page as signLoad
-import views.account_functions.accountIsTrue as accountIsTrue
 
 class Content(tk.CTkFrame):
     def __init__(self, master, width, height):
@@ -17,10 +15,4 @@ class Content(tk.CTkFrame):
 
         tk.CTkFrame(self, fg_color="#3F3A46", width=330, height=470).place(x=0, y=0)
         
-        if config.dct['user_activated'] is True:
-            pass
-        elif config.dct['user_activated'] is False:
-            contentx = signLoad.ContentSignIn(self, 330, 470)
-            contentx.place(x=10, y=10)
-        else:
-            print('Syntax error (src/views/settings/config.py {line 8})')
+        
--- src/views/settings/config.py
+++ src/views/settings/config.py
@@ -6,10 +6,22 @@ directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))[:-6]
 path = os.path.join(directory, "assets", "account_image.png")
 
 dct = {
-    'VERSION': 'Runance w1.02.04A',
-    'login': 'admin',
-    'password': 'admin',
-    'name': 'bssdka',
-    'profile_image': path,
-    'user_activated': False,
+    
+    "VERSION": 'Runance w1.02.04A',
+    
+    "admin": {
+        "id": '1',
+        "login": 'admin',
+        "password": 'admin',
+        "name": 'bssdka',
+        "profile_image": path,
+    },
+
+    "user": {
+        "id": '2',
+        "login": 'userL340',
+        "password": 'L340-LAPTOP',
+        "name": 'user',
+        "profile_image": path,
+    },
 }
